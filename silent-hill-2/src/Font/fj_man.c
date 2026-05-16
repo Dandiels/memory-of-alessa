4f390cb1e8d1369a373ee9a6e4958494e0e927af
moth dreams <mothdreams666@gmail.com>
2026-05-16 10:48:46 -0700

    sh2: match `fjFontDrawExecVif1`

--- silent-hill-2/src/Font/fj_man.c
+++ silent-hill-2/src/Font/fj_man.c
@@ -1,6 +1,32 @@
 #include "common.h"
 #include "Font/fj_man.h"
 
+INCLUDE_ASM("asm/nonmatchings/Font/fj_man", fjInitAll);
+
+INCLUDE_ASM("asm/nonmatchings/Font/fj_man", fjInitFontTexture);
+
+INCLUDE_ASM("asm/nonmatchings/Font/fj_man", fjMoveEffect);
+
+INCLUDE_ASM("asm/nonmatchings/Font/fj_man", fjDrawExec);
+
+INCLUDE_ASM("asm/nonmatchings/Font/fj_man", fjFontDrawExec);
+
+void fjFontDrawExecVif1(void) {
+    void* adr1; void* adr2;
+
+    d1cSend(fontTexLoad(sh2gfw_Get_BaseTBP0for2D(), FONT_TEX_CLUT_ADR));
+
+    
+    adr1 = fontFlush();
+    adr2 = mfontFlush();
+    
+    d1cSend(adr1);
+    d1cSend(adr2);
+    
+    d1cSend(fontAfterEnv());
+}
+
+
 int fjAssert_(const char *file, int line, const char *str) __attribute__((noreturn))
 {
     printf("assertion \"%s\" failed: file \"%s\", line %d\n", str, file, line);
--- silent-hill-2/src/Font/fj_man.h
+++ silent-hill-2/src/Font/fj_man.h
@@ -1,6 +1,7 @@
 #ifndef FJ_MAN_H
 #define FJ_MAN_H
 
+#define FONT_TEX_CLUT_ADR 0x3600
 #define fjAssert(_cond, _file, _line)    \
     if (_cond)                           \
     {                                    \
@@ -10,5 +11,19 @@
     {                                    \
         fjAssert_(_file, _line, #_cond); \
     }
+
+void fjInitAll(void);
+
+void fjInitFontTexture(void);
+
+void fjMoveEffect(void);
+
+void fjDrawExec(void);
+
+void fjFontDrawExec(void);
+
+void fjFontDrawExecVif1(void);
+
+int fjAssert_(const char *file, int line, const char *str);
 
 #endif
