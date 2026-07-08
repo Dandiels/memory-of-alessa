b30e3d1faf40e01634a233a6d553203c879ad4a4
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-08 04:19:48 +0200

    sh2: match `sh2_init` (#247)
    * Update sh2gfw_all_sysinit.h
    
    * Update movie_main.h
    
    * Update init_mt_sys.h
    
    * Update sh2_scf.h
    
    * Add libmc.h
    
    * Update sh2gfw_Texpacket.h
    
    * Update sh2gfw_md_spclmapping.h
    
    * Update kari_probe_draw.h
    
    * Update sh2gfw_util.h
    
    * Update pad.h
    
    * Add data_pic_etc.h
    
    * Update spack.h
    
    * Update picture.h
    
    * Update data_load.h
    
    * Add sh2_init.c
    
    * Update sh2gfw_drawloop_main.h
    
    * Update event.h
    
    * Update mem_share.h
    
    * Update dbfntprint.h
    
    * Update sh2gfw_all_sysinit.h
    
    * Update m3_maria.h
    
    * Update SLUS_202.28.yaml
    
    * Update demoview.h
    
    * Update dbfntprint.h
    
    * Match sh2_init.c

--- silent-hill-2/src/sh2gfw_all_sysinit.h
+++ silent-hill-2/src/sh2gfw_all_sysinit.h
@@ -1,4 +1,12 @@
 #ifndef SH2GFW_ALL_SYSINIT_H
 #define SH2GFW_ALL_SYSINIT_H
 
+#include "sh2_common.h"
+
+int Vcallback_test(int ca);
+void init_PS2(void);
+void step_init_ONE(void);
+void step_init_STAGE(void);
+void map_DATA_LOAD(void);
+
 #endif // SH2GFW_ALL_SYSINIT_H
